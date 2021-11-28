0254fd42034ee4859d959a94d6c40944e21617e7
kaira96 <kayratsagynbekov@gmail.com>
2021-11-28 21:14:40 +0600

    Смарт контракты

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from web3 import Web3
+import json
 
 
-infura_url = "https://mainnet.infura.io/v3/4d4c1fc5f36b4c7d8d643b27d8651138"
+# Смарт-контракты https://etherscan.io/
+
+infura_url = 'https://mainnet.infura.io/v3/4d4c1fc5f36b4c7d8d643b27d8651138'
 web3 = Web3(Web3.HTTPProvider(infura_url))
-
 print(web3.isConnected())
 
-print(web3.eth.blockNumber)
+abi = json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"withdrawEther","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"unfreeze","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"freezeOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"freeze","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"decimalUnits","type":"uint8"},{"name":"tokenSymbol","type":"string"}],"payable":false,"type":"constructor"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Freeze","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Unfreeze","type":"event"}]')
+address = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52" #https://etherscan.io/address/0xB8c77482e45F1F44dE1745F52C74426C631bDD52
 
-balance = web3.eth.getBalance("0xB3F20d336239A295D3FbEE056Fc304b959E2267e") #Узнать баланс с кошелька Metamask
-print(web3.fromWei(balance, 'ether'))
+contract = web3.eth.contract(address=address, abi=abi)
+print(contract)
+
+totalSupply = contract.functions.totalSupply().call()
+print(totalSupply)
+print(web3.fromWei(totalSupply, 'ether'))
+print(contract.functions.name().call())
+print(contract.functions.symbol().call())
+balance = contract.functions.balanceOf('0x424dbed38cd0d83b13e2a150f86710fb445680cb').call() # Не работает потом погуглю
+print(web3.fromWei(balance, 'ether')) # Не работает потом погуглю
