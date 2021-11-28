6f5ecaca4a16ceb4c21f3d4e775a5934ec0bc590
kaira96 <kayratsagynbekov@gmail.com>
2021-11-28 22:01:51 +0600

    Произведение транзакций

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
 from web3 import Web3
 import json
 
+# Транзакции - отправка эфира
+# https://www.trufflesuite.com/ganache
+# Скачать и установить
 
-# Смарт-контракты https://etherscan.io/
+ganache_url = "http://127.0.0.1:7545"
+web3 = Web3(Web3.HTTPProvider(ganache_url))
+# print(web3.isConnected())
 
-infura_url = 'https://mainnet.infura.io/v3/4d4c1fc5f36b4c7d8d643b27d8651138'
-web3 = Web3(Web3.HTTPProvider(infura_url))
-print(web3.isConnected())
 
-abi = json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"withdrawEther","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"unfreeze","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"freezeOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"freeze","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"decimalUnits","type":"uint8"},{"name":"tokenSymbol","type":"string"}],"payable":false,"type":"constructor"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Freeze","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Unfreeze","type":"event"}]')
-address = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52" #https://etherscan.io/address/0xB8c77482e45F1F44dE1745F52C74426C631bDD52
+account_1 = "0xf45b45466df862C039010AFc9741455DA46a277D"
+account_2 = "0xC2c965582938d06A90C1f7C6fA62b5961eC9E338"
 
-contract = web3.eth.contract(address=address, abi=abi)
-print(contract)
+private_key = "94d65d4717a1f3c7d16a426ef81ca9eb9637e4e896d84a1a0f077e0c5f967448"
 
-totalSupply = contract.functions.totalSupply().call()
-print(totalSupply)
-print(web3.fromWei(totalSupply, 'ether'))
-print(contract.functions.name().call())
-print(contract.functions.symbol().call())
-balance = contract.functions.balanceOf('0x424dbed38cd0d83b13e2a150f86710fb445680cb').call() # Не работает потом погуглю
-print(web3.fromWei(balance, 'ether')) # Не работает потом погуглю
+# get the nonce
+nonce = web3.eth.getTransactionCount(account_1)
+# build a transaction
+tx = {
+    "nonce" : nonce,
+    "to" : account_2,
+    "value" : web3.toWei(1, 'ether'),
+    "gas" : 2000000,
+    "gasPrice" : web3.toWei('50', 'gwei')
+
+}
+# sign transaction
+signed_tx = web3.eth.account.signTransaction(tx, private_key)
+# send transaction
+tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+# print(tx_hash) # После запуска проверить ganache там произвелась транзакция 1го эфира
+print(web3.toHex(tx_hash))
+# get tansaction hash
