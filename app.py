de737a2dc7acf6d5f36f4d3641488b2375dfd88f
kaira96 <kayratsagynbekov@gmail.com>
2021-11-28 19:40:27 +0600

    Получение баланса из https://infura.io/ и из кошелька metamask

---
+++ app.py
@@ -0,0 +1,12 @@
+from web3 import Web3
+
+
+infura_url = "https://mainnet.infura.io/v3/4d4c1fc5f36b4c7d8d643b27d8651138"
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+print(web3.isConnected())
+
+print(web3.eth.blockNumber)
+
+balance = web3.eth.getBalance("0xB3F20d336239A295D3FbEE056Fc304b959E2267e")
+print(web3.fromWei(balance, 'ether'))
